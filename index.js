ced9c9e0085f78b17e4df48a0a394896d8597db0
pedrinhobolado <148726387+pedrinhobolado@users.noreply.github.com>
2023-10-22 19:04:50 -0400

    Update index.js aaaaa

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = '';
 var password = '';
 var shared_secret = '';
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730,440,570,601510,1407200,230410,236390,438100,1920960,304930,1222670,1286830,386360,700330,1128810,578080,238960,444090,1869590,1625450,1599340,1353300,1049590,1085660,459820,1201240,1172470,714010,2357570,291550,363970];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
